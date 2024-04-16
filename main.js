a3038e9714d196a7c0a3f605ab35b51f85b65c6c
Nacaxi <107555990+nacaxi@users.noreply.github.com>
2024-04-16 08:01:31 -0300

    a

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const botoes = document.querySelectorAll(".botao")
 
-
-
 for(let i =0;i<botoes.length; i++){
-    console.log(i)
     botoes[i].onclick = function(){
-        console.log("teste")
+        for (let j=0;j<botoes.length; j++){
+            botoes[i].classList.remove("ativo");
+        }
         botoes[i].classList.add("ativo");
+
     }
 }
