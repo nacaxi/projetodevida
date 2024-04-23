4e3e37ef2c39c5d374921fe1af84b671932a86e0
Nacaxi <107555990+nacaxi@users.noreply.github.com>
2024-04-23 07:56:58 -0300

    a

--- main.js
+++ main.js
@@ -8,5 +8,7 @@ for(let i =0;i<botoes.length; i++){
         }
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
+        for (let j=0;j<botoes.length; j++){
+            botoes[j].classList.remove("ativo");
     }
 }
