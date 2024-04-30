55a16b0ab8a79740171265b1295dbc755867cd72
Nacaxi <107555990+nacaxi@users.noreply.github.com>
2024-04-30 07:41:08 -0300

    7

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const botoes = document.querySelectorAll(".botao")
 const textos = document.querySelectorAll(".aba-conteudo")
-const contador = document.querySelectorAll(".contador")
 
 for(let i =0;i<botoes.length; i++){
     botoes[i].onclick = function(){
@@ -12,3 +11,5 @@ for(let i =0;i<botoes.length; i++){
         textos[i].classList.add("ativo");
     }
 }
+
+const contador = document.querySelectorAll(".contador")
