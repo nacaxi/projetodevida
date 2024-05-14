84331a0641f8b506b571b384e4deeacb7b6fa99a
Nacaxi <107555990+nacaxi@users.noreply.github.com>
2024-05-14 07:47:37 -0300

    a

--- main.js
+++ main.js
@@ -25,4 +25,5 @@ function calculaTempo(tempoObjetivo){
     let minutos = Math.floor(segundos/60);
     let horas = Math.floor(minutos/60);
     let dias = Math.floor(horas/24);
+    return dias;
 }
