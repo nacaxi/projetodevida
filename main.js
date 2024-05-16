801131c7f3c4e2837150b2a974563395fbf79670
Nacaxi <107555990+nacaxi@users.noreply.github.com>
2024-05-15 22:12:14 -0300

    a

--- main.js
+++ main.js
@@ -16,10 +16,10 @@ for(let i =0;i<botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador")
-const tempoObjetivo1 = new Date("2024-05-20T00:00:00");
-const tempoObjetivo2 = new Date("2024-11-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-05-17T00:00:00");
+const tempoObjetivo2 = new Date("2024-07-01T00:00:00");
 const tempoObjetivo3 = new Date("2024-12-05T00:00:00");
-const tempoObjetivo4 = new Date("2025-01-05T00:00:00");
+const tempoObjetivo4 = new Date("2025-01-01T00:00:00");
 let tempoAtual = new Date()
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
